d929dbc26f5096e8b46398a5bf501b71b2cff2e7
muricd <Dmitry.Murich@emc.com>
2018-01-26 17:08:31 +0300

    Add image handling

--- teams_tv_server/teams_tv_coe.py
+++ teams_tv_server/teams_tv_coe.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+COE_PHOTOS = os.path.join('static', 'coe_photos')
 
 @app.route('/traffic')
-def show_index():
-    return render_template("index.html")
+def show_traffic():
+    return render_template("traffic.html")
+
+
+@app.route('/image')
+def show_image():
+    full_filename = os.path.join(COE_PHOTOS, 'Untitled.png')
+    return render_template("image.html", user_image=full_filename)
 
 
 if __name__ == '__main__':
