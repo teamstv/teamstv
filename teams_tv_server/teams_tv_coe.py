e134e01af16c421aa7810d860859608a31d17340
teamstv <teamstv@yandex.ru>
2018-01-26 17:27:29 +0300

    fixed calendar, added serialization for datetime, implemented REST-ish for event list

--- teams_tv_server/teams_tv_coe.py
+++ teams_tv_server/teams_tv_coe.py
@@ -1,4 +1,9 @@
+import json
+
 from flask import Flask, render_template
+
+from lib import calendar, misc
+import settings
 
 app = Flask(__name__)
 
@@ -6,6 +11,20 @@ app = Flask(__name__)
 @app.route('/traffic')
 def show_index():
     return render_template("index.html")
+
+
+@app.route("/test_json")
+def test_json():
+    calendars = calendar.connect(settings.CALDAV_USER, settings.CALDAV_PASSWORD, settings.CALDAV_URL)
+    data = calendar.get_current_events(calendars)
+    return json.dumps(data, cls=misc.DateTimeEncoder)
+
+
+@app.route("/events/current")
+def get_current_events():
+    calendars = calendar.connect(settings.CALDAV_USER, settings.CALDAV_PASSWORD, settings.CALDAV_URL)
+    data = calendar.get_current_events(calendars)
+    return json.dumps(data, cls=misc.DateTimeEncoder)
 
 
 if __name__ == '__main__':
