78336f4bb14cdfa6bff064a70bcfb0363f8cf276
Vashev <Sergey.Vashev@emc.com>
2018-01-27 15:09:47 +0300

    new clock without weather

--- teams_tv_server/static/js/clock.js
+++ teams_tv_server/static/js/clock.js
@@ -1,5 +1,5 @@
-function clockInit() {
-
+//function clockInit() {
+/*
     Element.Properties.transform = {
 
         set: function(transform){
@@ -143,6 +143,36 @@ function clockInit() {
     }
 
     setClock.periodical(1000);
+*/
+$(document).ready(function() {
+    /*var clock = new Vue({
+        el: '#clock',
+        data: {
+            time: '',
+            date: ''
+        }
+    })*/
+
+    var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    var timerID = setInterval(updateTime, 1000);
+    updateTime();
+    function updateTime() {
+        var cd = new Date();
+
+        var date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+        var time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+        $('#clock .time').text(time);
+        $('#clock .date').text(date);
+    };
+
+    function zeroPadding(num, digit) {
+        var zero = '';
+        for(var i = 0; i < digit; i++) {
+            zero += '0';
+        }
+        return (zero + num).slice(-digit);
+    }
+});
 
 
-}
+//
