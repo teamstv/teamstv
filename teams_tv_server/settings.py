256130544f66c16ca62e60dd7a6a2cb732efab1a
teamstv <teamstv@yandex.ru>
2018-01-27 12:29:11 +0300

    merged changes

--- teams_tv_server/settings.py
+++ teams_tv_server/settings.py
@@ -2,7 +2,6 @@ STATIC_FOLDER = "static"
 IMG_FOLDER = "img"
 CALDAV_URL = "caldav.yandex.ru"
 # TODO: fill in user/password
-CALDAV_USER = ""
-CALDAV_PASSWORD = ""
-#BOT_TOKEN = '463871639:AAEMtFe-8bxU2qEob3AvFKNYJUj7bBL52k4'
-BOT_TOKEN = ''
+BOT_TOKEN = '463871639:AAEMtFe-8bxU2qEob3AvFKNYJUj7bBL52k4'
+CALDAV_USER = "teamstv"
+CALDAV_PASSWORD = "Password123!"
