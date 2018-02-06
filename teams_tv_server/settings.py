c2378f166664c0adfa33bf06c14fc8406bf25d64
teamstv <teamstv@yandex.ru>
2018-02-06 15:54:58 +0300

    removed settings from repo

--- teams_tv_server/settings.py
+++ teams_tv_server/settings.py
@@ -2,10 +2,9 @@ STATIC_FOLDER = "static"
 IMG_FOLDER = "img"
 CALDAV_URL = "caldav.yandex.ru"
 # TODO: fill in user/password
-CALDAV_USER = "teamstv@yandex.ru"
-CALDAV_PASSWORD = "Password123!"
-#BOT_TOKEN = '463871639:AAEMtFe-8bxU2qEob3AvFKNYJUj7bBL52k4'
-BOT_TOKEN = ''
+CALDAV_USER = ""
+CALDAV_PASSWORD = ""
+BOT_TOKEN = '463871639:AAEMtFe-8bxU2qEob3AvFKNYJUj7bBL52k4'
 TIME_SHIFT = {
     'days': 2,
     'hours': -3
