a9f33957ab7da13207b15e581b753137a8f2e827
maiksaray <maiksaray@gmail.com>
2018-02-06 18:21:02 +0300

    added attempt to parse out rss links from html <a> tags accidentally put into description

--- teams_tv_server/lib/calendar.py
+++ teams_tv_server/lib/calendar.py
@@ -3,6 +3,56 @@ from datetime import datetime
 # Here we import local caldav so that we are not dependent on buggy version of the pip installation
 import caldav
 import pytz
+import re
+
+
+def increment_dead_kittens():
+    with open("dead_kittens", "r+") as f:
+        read = f.read()
+        if not read:
+            dead_kittens = 1
+        else:
+            dead_kittens = int(read) + 1
+        f.seek(0)
+        f.write(str(dead_kittens))
+        f.truncate()
+    print "{0} kittens died".format(dead_kittens)
+
+
+def extract_rss_feeds_from_html(description):
+    # if there are no signs of tags, do the usual thing
+    if ("<" not in description) and ">" not in description:
+        return json.loads(description)
+    # Ok, if we got here, the json contents are totally fucked up...
+    # There are mixed overlapping quotes and weird tags. We are trying to fix this
+    # The only solution I can imagine for it...
+    # REGEXPS. Now you folks have problems...
+    print "Tags found in rss description/configurations. attempting to extract links from tags. " \
+          "\nThis is plain stupid..." \
+          "\nDon't do this. A kitten dies every time this happens" \
+          "\nYou know what? I'm counting..."
+    increment_dead_kittens()
+    # all contents from start of <a> tag to it's close,
+    # except if there are many <a> tags, they must delimited with comma
+    # and we don't want to grap from start of the first one to the end of the last one
+    tag_regex = "<a[^,]*</a>"
+    tags = re.findall(tag_regex, description)
+    # positive look-behind of 'blocks":[',then any numbers delimited by comma and positive lookahead of ']'
+    blocks_regex = "(?<=blocks\"\:\[)[\d,]*(?=\])"
+    blocks_match = re.search(blocks_regex, description)
+    if blocks_match is None:
+        raise Exception("can't fucking parse fucking tags in fucking rss description")
+    blocks_str = blocks_match.group()
+    content = {"blocks": [int(t) for t in blocks_str.split(",")],
+               "data": {
+                   "feeds": []
+               }}
+    # positive look-behind of 'href="',then anything and positive lookahead of '">'
+    # So we match everything in between 'href="' and '">'
+    url_regex = "(?<=href=\").*(?=\">)"
+    for tag in tags:
+        content["data"]["feeds"].extend(re.findall(url_regex, tag))
+    return content
 
 
 def connect(user, password, url):
@@ -37,7 +87,11 @@ def get_events(calendars, time_from, time_to):
             start = e.dtstart.value
             end = e.dtend.value
             try:
-                contents = json.loads(e.description.value)
+                description = e.description.value
+                if summary == "rss":
+                    contents = extract_rss_feeds_from_html(description)
+                else:
+                    contents = json.loads(description)
             except Exception as err:
                 contents = {"blocks": [], "data": {}}
             blocks = contents["blocks"]
@@ -70,4 +124,3 @@ def get_current_events(calendars):
     :return ([{event}]): list of obtained events for current moment
     """
     return get_events(calendars, get_now(), get_now())
-
